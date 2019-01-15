fe82f6bc2fdd9ea8f4929d37f0f7633f568709f5
liushaoyong <tacitysl@sina.com>
2019-01-15 11:53:04 +0800

    刪除截取十條

--- src/main/java/com/saler/service/DirectionService.java
+++ src/main/java/com/saler/service/DirectionService.java
@@ -48,7 +48,7 @@ public class DirectionService extends LoggerUtil{
 		if(null!=beginTime&&null!=endTime) {
 			example.createCriteria().andGreaterThanOrEqualTo("salesDate",beginTime).andLessThanOrEqualTo("salesDate",endTime);
 		}
-		List<Direction> list=dm.selectByExample(example).subList(0, 10);
+		List<Direction> list=dm.selectByExample(example);
 
 		/*try {
 			async.addMysqlDirction(restTemplate, list, timeVersion);
